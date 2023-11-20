b4fbce9da5d3cbf8d6f4a958fa3eba3e8d5df523
peterbandu527 <123404048+peterbandu527@users.noreply.github.com>
2023-11-20 16:06:22 -0400

    Actualizacion

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,37 +1,38 @@
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0" style={{backgroundImage: `url(${`${import.meta.env.BASE_URL}`}/public/images/cabecera_1.png)`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', height: '150px'}}>
-      <div className="container px-1">
-        {/* <a className="navbar-brand font-weight-bolder ms-lg-0 " href="https://www.creative-tim.com/astro">UMC</a> */}
-        <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon mt-2">
-            <span className="navbar-toggler-bar bar1"></span>
-            <span className="navbar-toggler-bar bar2"></span>
-            <span className="navbar-toggler-bar bar3"></span>
-          </span>
-        </button>
-        <div className="collapse navbar-collapse" id="navigation">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="/astro-ecommerce/">
-                All Components
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link text-dark dropdown-toggle font-weight-bold d-flex align-items-center me-2 " aria-current="page" id="pagesExample" data-bs-toggle="dropdown" aria-expanded="false">
-                Pages
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="pagesExample">
-                <li><a className="dropdown-item" href="/web_umc/landing/">Landing Page</a></li>
-                <li><a className="dropdown-item" href="/web_umc/product/">Product Page</a></li>
-                <li><a className="dropdown-item" href="/web_umc/shopping-cart/">Shopping Cart</a></li>
-              </ul>
-            </li>
-            
-          </ul>
-        </div>
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div className="container-fluid">      
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarColor02">
+        <ul className="navbar-nav me-7 mb-2 mb-lg-0">
+          <li className="nav-item me-5">
+            <a className="nav-link active" aria-current="page" href="/web_umc/">Inicio</a>
+          </li>
+          <li className="nav-item me-5">
+            <a className="nav-link" href="#">Servicios</a>
+          </li>
+          <li className="nav-item me-5">
+            <a className="nav-link" href="#">Noticias</a>
+          </li>
+          <li className="nav-item me-5">
+            <a className="nav-link" href="#">Dependencias</a>
+          </li>
+          <li className="nav-item me-5">
+            <a className="nav-link" href="#">Correo</a>
+          </li>
+        </ul>
+        <form className="d-flex">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          {/* <button className="btn btn-outline-light" type="submit">Search</button> */}
+        </form>
       </div>
-    </nav>
+    </div>
+  </nav>
+    </div>
+    
   );
 };
 
